fd62ccd5a0077eae7051a8f1e782a37cf41204c4
Ryan079 <ryan079077@gmail.com>
2025-04-06 15:05:48 -0300

    ProjetoAed2

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,9 @@
 import Algoritmo.AlgoritmoDijkstra;
-import Algoritmo.AlgoritmoVizinhoMaisProximo;
-import Algoritmo.TSPSubgrafo;
 import Grafo.GrafoLogistica;
 import Grafo.LeitorCSV;
 
 import java.io.IOException;
-import java.util.HashSet;
-import java.util.List;
 import java.util.Scanner;
-import java.util.Set;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -18,19 +13,12 @@ public class Main {
         GrafoLogistica grafo = new GrafoLogistica();
         grafo = LeitorCSV.carregarGrafo("src/conexoes_pernambuco.csv");
 
-        System.out.println("Mapa de Cidades:");
-        grafo.getMapaCidades().forEach((cidade, estradas) -> {
-            System.out.println("Cidade: " + cidade.getNome());
-            estradas.forEach(e -> System.out.println("  -> " + e.getDestino().getNome() + " (" + e.getDistancia() + ")"));
-        });
 
 
         while(true) {
             System.out.println("\n--- MENU DO SISTEMA ---");
             System.out.println("1. Exibir cidades");
             System.out.println("2. Exibir menor caminho entre duas cidades");
-            System.out.println("3. Percorrer estado.");
-            System.out.println("4. Realizar viagem múltipla.");
             System.out.println("0. Sair");
             System.out.println("Escolha uma opção");
 
@@ -48,45 +36,6 @@ public class Main {
                     String destino = input.nextLine();
                     AlgoritmoDijkstra.encontrarMenorRota(grafo, origem, destino);
                     break;
-                case 3:
-                    System.out.println("Cidade inicial: ");
-                    String cidade = input.nextLine();
-                    try {
-                        List<String> rota = AlgoritmoVizinhoMaisProximo.calcularRotaMaisProxima(grafo, cidade);
-                        System.out.println("Rota: " + String.join(" -> ", rota));
-                    } catch(IllegalArgumentException e) {
-                        System.out.println(e.getMessage());
-                    }
-                    break;
-                case 4:
-                    System.out.println("Insira as cidades das encomendas que precisam ser entregues (digite fim para encerrar)");
-                    Set<String> cidadesSelecionadas = new HashSet<>();
-                    while(true) {
-                        String entrada = input.nextLine().trim();
-                        if(entrada.equalsIgnoreCase("fim"))
-                            break;
-                        if(grafo.encontrarCidade(entrada) != null)
-                            cidadesSelecionadas.add(entrada);
-                        else
-                            System.out.println("Cidade não encontrada. Tente novamente");
-                    }
-                    if(cidadesSelecionadas.size() < 2) {
-                        System.out.println("Adicione pelo menos duas cidades.");
-                        return;
-                    }
-
-                    System.out.println("Digite a cidade inicial: ");
-                    String inicio = input.nextLine().trim();
-
-                    try {
-                        List<String> rota = TSPSubgrafo.resolucaoTSP(grafo, cidadesSelecionadas, inicio);
-
-                        System.out.println("\nRota encontrada: ");
-                        for(String c : rota)
-                            System.out.println(c + " -> ");
-                    } catch(Exception e) {
-                        System.out.println("Erro ao executar algoritmo: " + e.getMessage());
-                    }
                 case 0:
                     break;
                 default:
